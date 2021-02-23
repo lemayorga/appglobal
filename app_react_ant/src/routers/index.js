55010e785e00c5b5019cf078ce7543129b72229f
Juan Carlos Aleman Mayorga <jmayorga92@hotmail.com>
2021-02-22 23:01:47 -0600

    rutas de la aplicacion

--- app_react_ant/src/routers/index.js
+++ app_react_ant/src/routers/index.js
@@ -8,17 +8,10 @@ const AppRouter = () =>(
     // <Router history={ history }>
     // <Router >
         <Switch>
-          {/* <PublicRoute restricted={true} component={Login} path={routes.login} exact /> */}
-
-
           <Route>
             <LayoutMain>
               <Switch>
-
                 <Route exact path={["/", "/roles"]} component={RolesPage} />
-                {/* <Route exact path="/add" component={AddTutorial} />
-                <Route path="/tutorials/:id" component={Tutorial} /> */}
-
               </Switch>
             </LayoutMain>
           </Route>
